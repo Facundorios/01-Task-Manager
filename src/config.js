ed6bdfbcbc3bba53491556bb63e6172d6810b76e
francobustos23 <bustosfranco071@gmail.com>
2024-10-23 16:26:52 -0300

    deletion of unnecessary files

--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const TOKEN_SECRET_KEY = "tokenSecret1121";
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-import { databaseConnection } from "./db.js";
-
-databaseConnection();
-app.listen(3000);
-console.log("listening on port 3000");
