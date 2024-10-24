372e7b5f4102409296b7068d4814b193cb09567c
francobustos23 <bustosfranco071@gmail.com>
2024-10-23 23:45:57 -0300

    jwt environment variable added

--- server/src/environments/environments.ts
+++ server/src/environments/environments.ts
@@ -10,4 +10,5 @@ export const envs = {
     DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
     DB_NAME: env.get('DB_NAME').required().asString(),
     DB_DIALECT: process.env.DB_DIALECT as Dialect || 'mysql',
+    JWT_SECRET_KEY: env.get('JWT_SECRET_KEY').required().asString(),
 }
